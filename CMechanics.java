09c187d13184739191f3f85acca3ff081997281c
JSztyrak <j.sztyrak96@gmail.com>
2019-04-23 16:35:02 +0200

    syntax fixes

--- CMechanics.java
+++ CMechanics.java
@@ -11,32 +11,21 @@ import static ukladankav1.UkladankaV1.Window;
 
 public final class CMechanics extends JFrame implements ActionListener {
     
-    long start, stop;
+    private int _lenght; 
     
-    JButton up, down, left, right; 
+    private int[][] _field; 
     
-    private int lenght; 
+    private int[] _vector; 
     
-    private int[][] field; 
-    
-    private int[] vector; 
-    
-    private int X, Y; 
+    private int X, Y;
+	
+	JButton up, down, left, right; 
+		
+	private long _start, _stop;
    
-    private static JButton createButton(String text,int x, int y, int szer, int wys){
-        
-        JButton button = new JButton(text);
-        button.setBounds(x,y,szer,wys);
-        button.setBorderPainted(false);
-        button.setFocusPainted(false);
-        button.setForeground(Color.BLACK);
-      
-        return button;
-    }
-
     public CMechanics(){
       
-        start = System.currentTimeMillis();
+        _start = System.currentTimeMillis();
        
         right = createButton("right",200,303,97,94);
         Window.add(right);
@@ -54,15 +43,26 @@ public final class CMechanics extends JFrame implements ActionListener {
         Window.add(down);
         down.addActionListener(this);
        
-        lenght = 4;
-        field = new int [lenght][lenght];
-        vector = new int [16];
+        _lenght = 4;
+        _field = new int [_lenght][_lenght];
+        _vector = new int [16];
 
          do{
             shuffle();
-         }while(IsSolvable(vector)==false);        
+         }while(IsSolvable(_vector)==false);        
     }
    
+       private static JButton createButton(String text,int x, int y, int szer, int wys){
+        
+        JButton button = new JButton(text);
+        button.setBounds(x,y,szer,wys);
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.setForeground(Color.BLACK);
+      
+        return button;
+    }
+	
     private void shuffle(){
             
         Random rand = new Random();
@@ -72,9 +72,9 @@ public final class CMechanics extends JFrame implements ActionListener {
         int n = 0;
         int z = 0;
            
-        for(int i=0; i<lenght; i++){
+        for(int i=0; i<_lenght; i++){
             for(int j=0; j<4; j++ )
-            field[i][j] = 0;
+            _field[i][j] = 0;
         }
             
         while(n<16){
@@ -82,50 +82,50 @@ public final class CMechanics extends JFrame implements ActionListener {
             col = rand.nextInt(4);
             row = rand.nextInt(4);
                
-            if(field[row][col] == 0){
+            if(_field[row][col] == 0){
                 if(counter == 16){
-                    field[row][col] = 0;
+                    _field[row][col] = 0;
                     X = row;
                     Y = col;
                     break;
                 }  
             else{
-                field[row][col] = counter;
+                _field[row][col] = counter;
                 ++counter;
                 n++;
                 }
             }  
         }
             
-        for(int k = 0; k<lenght; k++){
-            for(int l=0; l<lenght; l++)      
-                vector[z++] = field[k][l];  
+        for(int k = 0; k<_lenght; k++){
+            for(int l=0; l<_lenght; l++)      
+                _vector[z++] = _field[k][l];  
         }
     }
 
-    public boolean IsSolvable(int[] vector){
+    public boolean IsSolvable(int[] _vector){
         
         int parity = 0;
         int row = 0;
         int emptyRow = 0;
             
-        for (int i = 0; i < vector.length; i++){
+        for (int i = 0; i < _vector.length; i++){
             
-            if (i % lenght == 0)
+            if (i % _lenght == 0)
                 row++;
             
-            if (vector[i] == 0){ 
+            if (_vector[i] == 0){ 
               emptyRow = row;
               continue;
             }
             
-            for (int j = i + 1; j < vector.length; j++){
-                if (vector[i] > vector[j] && vector[j] != 0)
+            for (int j = i + 1; j < _vector.length; j++){
+                if (_vector[i] > _vector[j] && _vector[j] != 0)
                     parity++;
             }
         }
     
-        if (lenght % 2 == 0) {
+        if (_lenght % 2 == 0) {
             if (emptyRow % 2 == 0)
                 return parity % 2 == 0;
            
@@ -138,46 +138,46 @@ public final class CMechanics extends JFrame implements ActionListener {
 
     private void GoRight() {
         if( Y > 0 ) {
-            field[X][Y] = field[X][Y-1];
-            field[X][Y-1] = 0;
+            _field[X][Y] = _field[X][Y-1];
+            _field[X][Y-1] = 0;
             Y--;
         } 
     }
         
     private void GoLeft() {
-        if( Y < lenght -1 ){
-            field[X][Y] = field[X][Y+1];
-            field[X][Y+1] = 0;
+        if( Y < _lenght -1 ){
+            _field[X][Y] = _field[X][Y+1];
+            _field[X][Y+1] = 0;
             Y++;
         }
     }
         
     private void GoUp() {
-        if( X < lenght -1 ){
-            field[X][Y] = field[X+1][Y];
-                field[X+1][Y] = 0;
+        if( X < _lenght -1 ){
+            _field[X][Y] = _field[X+1][Y];
+                _field[X+1][Y] = 0;
                 X++;
         }
     }
          
     private void GoDown() {
         if( X > 0 ){
-            field[X][Y] = field[X-1][Y];
-            field[X-1][Y] = 0;
+            _field[X][Y] = _field[X-1][Y];
+            _field[X-1][Y] = 0;
             X--;
         }
     }
     
     public int GetValue(int x, int y) {
-        if(X >= 0  && X <= lenght - 1 && Y >= 0 && Y <= lenght-1){
-            return field[x][y];
+        if(X >= 0  && X <= _lenght - 1 && Y >= 0 && Y <= _lenght-1){
+            return _field[x][y];
         }
             return 0;
     }
     
     public void infoBox(){
-        stop = System.currentTimeMillis();
-        long czas = (stop-start)/1000;
+        _stop = System.currentTimeMillis();
+        long czas = (_stop-_start)/1000;
         JOptionPane.showMessageDialog(null,"GRATULACJE!  Ukończyłeś grę w czasie: " + czas + " sekund","KONIEC GRY", JOptionPane.INFORMATION_MESSAGE);
     }
     
@@ -185,9 +185,9 @@ public final class CMechanics extends JFrame implements ActionListener {
         
         int counter = 1;
         
-        for(int i=0; i<lenght; i++){
-            for(int j=0; j<lenght; j++){
-                if (field[i][j] == counter || (i == 3 && j == 3))
+        for(int i=0; i<_lenght; i++){
+            for(int j=0; j<_lenght; j++){
+                if (_field[i][j] == counter || (i == 3 && j == 3))
                     counter++;
                 else
                     return false;
